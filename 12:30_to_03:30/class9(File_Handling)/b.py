9c59bb914120e57d7b276af99038a09a03be03c1
Muhammad qasim <m.qasim077@gmail.com>
2020-01-27 13:01:22 +0500

    File Handling (Text, Audio, Image and Videos)

---
+++ 12:30_to_03:30/class9(File_Handling)/b.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture("mov_bbb.mp4")
+ret, frame = cap.read()
+while(1):
+   ret, frame = cap.read()
+   cv2.imshow('frame',frame)
+   if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+       cap.release()
+       cv2.destroyAllWindows()
+       break
+   cv2.imshow('frame',frame)
